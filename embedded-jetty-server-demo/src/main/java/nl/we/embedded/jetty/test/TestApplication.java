161a999dc0d13c9fab4b4acead94d7e180d0fd26
WillemElbers <willem@clarin.eu>
2016-08-29 20:19:30 +0200

    Cleanup unused code

--- embedded-jetty-server-demo/src/main/java/nl/we/embedded/jetty/test/TestApplication.java
+++ embedded-jetty-server-demo/src/main/java/nl/we/embedded/jetty/test/TestApplication.java
@@ -1,9 +1,5 @@
 package nl.we.embedded.jetty.test;
 
-import io.swagger.jaxrs.config.BeanConfig;
-import nl.we.embedded.jetty.ServerConfig;
-import nl.we.jersey.filters.CORSFilter;
-import org.glassfish.jersey.server.ResourceConfig;
 import nl.we.embedded.jetty.rest.DefaultApplication;
 
 /**
@@ -16,17 +12,6 @@ public class TestApplication extends DefaultApplication {
     
     public TestApplication() {
         super(PACKAGES);
-        /*
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.0");
-        beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:"+ServerConfig.getInstance().getPort());
-        beanConfig.setBasePath("/");
-        beanConfig.setResourcePackage("nl.we.embedded.jetty.test.resources");
-        beanConfig.setScan(true);
-        */
-        //packages("io.swagger.jaxrs.listing");
         packages(PACKAGES);
-        //register(CORSFilter.class);
     }
 }
