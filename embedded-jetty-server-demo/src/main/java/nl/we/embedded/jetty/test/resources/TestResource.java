cb48fd0d07605146c3301d11b0b48dfccce3045d
WillemElbers <willem@clarin.eu>
2016-09-10 08:47:57 +0200

    Added second test endpoint

--- embedded-jetty-server-demo/src/main/java/nl/we/embedded/jetty/test/resources/TestResource.java
+++ embedded-jetty-server-demo/src/main/java/nl/we/embedded/jetty/test/resources/TestResource.java
@@ -2,8 +2,10 @@ package nl.we.embedded.jetty.test.resources;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -26,4 +28,27 @@ public class TestResource {
         return model;
     }
     
+    @Path("/test2")
+    @GET
+    @ApiOperation(
+        value = "Second test endpoint",
+        notes = "Just return a simple message to show this is working as well",
+        response = TestModel.class)
+    public TestModel get2() {
+        TestModel model = new TestModel(true, "this works too");
+        return model;
+    }
+    
+    /*
+    @Path("/test3")
+    @GET
+    @ApiOperation(
+        value = "Third test endpoint with parameter",
+        notes = "Just return a simple message to show this is working as well",
+        response = TestModel.class)
+    public TestModel get3(@ApiParam(value = "x", required = true) @PathParam("x") String x) {
+        TestModel model = new TestModel(true, "this works too, argument x = ["+x+"]");
+        return model;
+    }
+    */
 }
